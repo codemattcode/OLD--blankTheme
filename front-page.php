3757c3c4643d329675a08d81d81cd35906a4b996
codemattcode <codemattcode@gmail.com>
2017-03-07 04:19:12 +0000

    refactor codebase

--- front-page.php
+++ front-page.php
@@ -1,3 +1,4 @@
+<!-- Header including Meta Info -->
 <?php  
   
 get_header();
@@ -5,80 +6,29 @@ get_header();
   if (have_posts()) :
     while (have_posts()) : the_post(); 
 
-  the_content();
+  // the_content();
 ?>
 
-
-    <div class="wrapper">
-<div class="row">
-  
-  <div class="col-6">
-    
-    <?php if (is_active_sidebar('section-one-hero')) : ?>
-      <div class="widget">
-        <?php dynamic_sidebar('section-one-hero'); ?>
-      </div>
-    <?php endif; ?>
-    
-  </div>
-
-  <div class="col-6">
-<?php the_content(); ?>
-  </div>
-
-</div><!-- /row -->
-</div><!-- /wrapper -->
-
-
-
-<div class="wrapper">
-<div class="row">
-
-<!-- conditional logic whether to display this custom content -->
-
-    <?php if (get_theme_mod('ct-footer-callout-display') == "Yes") { ?>
-          
-        <div class="footer-callout">
-          <div class="col-6">    
-        <div class="footer-callout-image">
-        <img src="<?php echo wp_get_attachment_url(get_theme_mod('ct-footer-callout-image')) ?>">
-          </div> 
-        </div>
         
-          <div class="col-6">    
-        <div class="footer-callout-text">
-        <h2 class="callout-h2"><a href="<?php echo get_permalink(get_theme_mod('ct-footer-callout-link')) ?>">
-        <?php echo get_theme_mod('ct-footer-callout-headline') ?>
-        </a></h2>
-        <?php echo wpautop(get_theme_mod('ct-footer-callout-text')) ?>
-        </div>
-          </div>
-        </div></?php>
-
-    <?php } ?>
-<!-- /conditional logic whether to display this custom content -->
-
-</div><!-- /row -->
-</div><!-- /wrapper -->
+<!-- Page content -->
+<?php the_content(); ?>
+  
 
 
 
-
+<!-- Error if page not found -->
 <?php   
   endwhile;
 
   else : 
-    echo '<p>Page Not Found. Please try again</p>';
+    echo '<p>Oops! 404 : Page Not Found</p>';
 
-  endif; ?>
-
-
-
-<?php 
-
-get_footer();
-
+  endif; 
 ?>
+
+
+<!--  Footer content -->
+<?php get_footer(); ?>
 
 
 
